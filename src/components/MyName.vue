67eb5351040af40a86623b6cb482f1f3dd439578
saran <skan0077@student.monash.edu>
2024-08-06 16:24:37 +1000

    intial build

---
+++ src/components/MyName.vue
@@ -0,0 +1,10 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #heading>Welcome! sai saran kandala sesha maruthi sathya</template>
+  </WelcomeItem>
+</template>
+  
